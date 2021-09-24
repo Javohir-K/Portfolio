29292872156bf8322ae0cf40a5205ea5af3300c2
Javohir <61010500+Javohir-K@users.noreply.github.com>
2021-09-24 11:11:27 +0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $('.myimage').addClass('transform-y')
  $('.socials').addClass('transform-y')
  $('.sec2-middle').addClass('transform-y')
-  $('.section').addClass('transform-x')
+  // $('.section').addClass('transform-x')
  $('.navbar').addClass('transform-y')
  $('.sec3toptitle').addClass('transform-y')
  $('.sec2toptitle').addClass('transform-y')
@@ -16,11 +16,12 @@ $(document).ready(function(){
   $('.swiper').addClass('transform-x-swiper')
  $('.sec3-text').addClass('transform-y')
  $('.lang').addClass('transform-y')
-
+  $('.bigtext').addClass('bigtxt-translate-y')
 })
 
 $(document).mousemove(function (e) {
   $('.pointer').css({ left: e.pageX, top: e.pageY })
+
 })
 
 $('img').attr('draggable', false);
@@ -50,4 +51,17 @@ $('div').click(function (e) {
       top: y + 'px',
       left: x + 'px'
     }).addClass('rippleEffect');
+})
+
+
+$('.nav-menu-little').click(function(){
+  if($('.nav-menu').hasClass('nav-active')){
+    $('.nav-menu').css({left:'0'})
+    $('.nav-menu').removeClass('nav-active')
+  }
+  else{
+    $('.nav-menu').css({left:'-100%'})
+    $('.nav-menu').addClass('nav-active')
+
+  }
 })
